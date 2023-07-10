0581b0b06b7cd412be3e4036d64cff924cc0cd02
Kykal <benavides.alan@hotmail.com>
2023-07-09 20:04:13 -0600

    Create Divider.tsx

---
+++ components/ui/Divider.tsx
@@ -0,0 +1,12 @@
+//Main component content
+const Divider = (): JSX.Element => {
+	//Main component render
+	return (
+		<hr
+			className='border-t border-neutral-200'
+		/>
+	);
+};
+
+
+export default Divider; //Export main component
