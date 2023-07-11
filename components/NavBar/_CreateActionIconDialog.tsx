417e67dc9d7d81d42f9dc2f327c1abdfb2cbc10f
Kykal <benavides.alan@hotmail.com>
2023-07-11 06:14:54 -0600

    Document undefined fix?

--- components/NavBar/_CreateActionIconDialog.tsx
+++ components/NavBar/_CreateActionIconDialog.tsx
@@ -45,73 +45,6 @@ const CreateActionIconDialog = () => {
 		setIsOpen(false);
 	};
 
-	const target = document.body;
-	const content = (
-		<Dialog
-			open={isOpen}
-			onClose={closeHandler}
-			className='rounded-md p-0 container max-w-2xl w-auto'
-			aria-label='Create dialog'
-			aria-description='Upload multimedia to post'
-		>
-			<header
-				className='relative p-2'
-				aria-label='Header'
-			>
-				<h1
-					className='text-center font-medium'
-					title='Create new post'
-					aria-label='Create new post'
-				>
-					Create new post
-				</h1>
-				<ActionIcon
-					onClick={closeHandler}
-					className='absolute right-4 top-2'
-					title='Close create dialog'
-					aria-label='Close create dialog button'
-					aria-description='Closes actual dialog'
-				>
-					<MdClose
-						className='fill-neutral-400 text-xl'
-						aria-label='Close icon'
-					/>
-				</ActionIcon>
-			</header>
-			<Divider
-				aria-label='Divider'
-				aria-description='Divides header from main'
-			/>
-			<main
-				className='p-2 flex flex-col gap-2 items-center justify-center h-96'
-				aria-label='Dialog main container'
-			>
-				<div
-					className='relative flex text-8xl'
-				>
-					<LiaPhotoVideoSolid
-						title='Pictures and videos'
-						aria-label='Pictures and videos'
-					/>
-				</div>
-				<span
-					className='text-xl font-light'
-					title='Drag here pictures and videos'
-					aria-label='Drag here pictures and videos'
-				>
-					Drag here pictures and videos
-				</span>
-				<button
-					className='bg-sky-500 text-sm rounded-md px-4 py-2 text-white font-medium active:bg-sky-700'
-					title='Select from computer'
-					aria-label='Select from computer'
-				>
-					Select from computer
-				</button>
-			</main>
-		</Dialog>
-	);
-
 	return(
 		<>
 			<li>
@@ -125,9 +58,71 @@ const CreateActionIconDialog = () => {
 					<BsPlusSquare className='text-2xl' />
 				</ActionIcon>
 			</li>
-			{createPortal(
-				content,
-				target,
+			{typeof window !== "undefined" && createPortal(
+				<Dialog
+					open={isOpen}
+					onClose={closeHandler}
+					className='rounded-md p-0 container max-w-2xl w-auto'
+					aria-label='Create dialog'
+					aria-description='Upload multimedia to post'
+				>
+					<header
+						className='relative p-2'
+						aria-label='Header'
+					>
+						<h1
+							className='text-center font-medium'
+							title='Create new post'
+							aria-label='Create new post'
+						>
+							Create new post
+						</h1>
+						<ActionIcon
+							onClick={closeHandler}
+							className='absolute right-4 top-2'
+							title='Close create dialog'
+							aria-label='Close create dialog button'
+							aria-description='Closes actual dialog'
+						>
+							<MdClose
+								className='fill-neutral-400 text-xl'
+								aria-label='Close icon'
+							/>
+						</ActionIcon>
+					</header>
+					<Divider
+						aria-label='Divider'
+						aria-description='Divides header from main'
+					/>
+					<main
+						className='p-2 flex flex-col gap-2 items-center justify-center h-96'
+						aria-label='Dialog main container'
+					>
+						<div
+							className='relative flex text-8xl'
+						>
+							<LiaPhotoVideoSolid
+								title='Pictures and videos'
+								aria-label='Pictures and videos'
+							/>
+						</div>
+						<span
+							className='text-xl font-light'
+							title='Drag here pictures and videos'
+							aria-label='Drag here pictures and videos'
+						>
+							Drag here pictures and videos
+						</span>
+						<button
+							className='bg-sky-500 text-sm rounded-md px-4 py-2 text-white font-medium active:bg-sky-700'
+							title='Select from computer'
+							aria-label='Select from computer'
+						>
+							Select from computer
+						</button>
+					</main>
+				</Dialog>,
+				document.body,
 			)}
 		</>
 	);
