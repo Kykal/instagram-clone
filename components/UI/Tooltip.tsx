55c36e3022028b598e21da2cab74adb33528bcd9
Kykal <benavides.alan@hotmail.com>
2023-07-11 06:55:25 -0600

    Update Tooltip.tsx

--- components/UI/Tooltip.tsx
+++ components/UI/Tooltip.tsx
@@ -1,5 +1,9 @@
+//React
+import { HTMLAttributes } from 'react';
+
+
 //Typings
-type Tooltip = {
+type Tooltip = HTMLAttributes<HTMLDivElement> & {
 	children: JSX.Element;
 	label: string;
 }
@@ -10,16 +14,24 @@ const Tooltip = (props: Tooltip): JSX.Element => {
 
 	const baseTooltipClassName = 'z-10 absolute mx-auto p-2 bg-white shadow rounded-md text-sm top-1 transition-all duration-100 left-16 scale-0 peer-hover:scale-100 origin-left';
 
+	const { className, label, children, ...propsAttributes } = props;
+
+	const _className = className
+		? `${className} ${baseTooltipClassName}`
+		: baseTooltipClassName;
+
+
 	//Main component render
 	return (
 		<div
 			className='relative'
+			{...propsAttributes}
 		>
-			{props.children}
+			{children}
 			<span
-				className={baseTooltipClassName}
+				className={_className}
 			>
-				{props.label}
+				{label}
 			</span>
 		</div>
 	);
