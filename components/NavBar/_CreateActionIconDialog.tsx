a4605eadfe56e1099e8b2d5b438de32c000b24bf
Kykal <benavides.alan@hotmail.com>
2023-07-11 05:26:55 -0600

    Granular

---
+++ components/NavBar/_CreateActionIconDialog.tsx
@@ -0,0 +1,136 @@
+//React
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+
+//MATERIAL DESIGN
+//Components
+import ActionIcon from "../ui/ActionIcon";
+import Divider from "../ui/Divider";
+//Icons
+import {
+	BsPlusSquare,
+	BsPlusSquareFill,
+} from 'react-icons/bs';
+import {
+	LiaPhotoVideoSolid,
+} from 'react-icons/lia';
+import {
+	MdClose,
+} from 'react-icons/md';
+
+
+//Components
+import Dialog from 'native-react-dialog';
+
+
+//Typings
+type CreateActionIconDialog = {
+	
+}
+
+
+//Main component content
+const CreateActionIconDialog = () => {
+
+	//React
+	const [ isOpen, setIsOpen ] = useState<boolean>(false);
+
+	//Handlers
+	const openHandler = () => {
+		setIsOpen(true);
+	};
+
+	const closeHandler = () => {
+		setIsOpen(false);
+	};
+
+	const target = document.body;
+	const content = (
+		<Dialog
+			open={isOpen}
+			onClose={closeHandler}
+			className='rounded-md p-0 container max-w-2xl w-auto'
+			aria-label='Create dialog'
+			aria-description='Upload multimedia to post'
+		>
+			<header
+				className='relative p-2'
+				aria-label='Header'
+			>
+				<h1
+					className='text-center font-medium'
+					title='Create new post'
+					aria-label='Create new post'
+				>
+					Create new post
+				</h1>
+				<ActionIcon
+					onClick={closeHandler}
+					className='absolute right-4 top-2'
+					title='Close create dialog'
+					aria-label='Close create dialog button'
+					aria-description='Closes actual dialog'
+				>
+					<MdClose
+						className='fill-neutral-400 text-xl'
+						aria-label='Close icon'
+					/>
+				</ActionIcon>
+			</header>
+			<Divider
+				aria-label='Divider'
+				aria-description='Divides header from main'
+			/>
+			<main
+				className='p-2 flex flex-col gap-2 items-center justify-center h-96'
+				aria-label='Dialog main container'
+			>
+				<div
+					className='relative flex text-8xl'
+				>
+					<LiaPhotoVideoSolid
+						title='Pictures and videos'
+						aria-label='Pictures and videos'
+					/>
+				</div>
+				<span
+					className='text-xl font-light'
+					title='Drag here pictures and videos'
+					aria-label='Drag here pictures and videos'
+				>
+					Drag here pictures and videos
+				</span>
+				<button
+					className='bg-sky-500 text-sm rounded-md px-4 py-2 text-white font-medium active:bg-sky-700'
+					title='Select from computer'
+					aria-label='Select from computer'
+				>
+					Select from computer
+				</button>
+			</main>
+		</Dialog>
+	);
+
+	return(
+		<>
+			<li>
+				<ActionIcon
+					className={`w-full border border-transparent`}
+					activeIcon={<BsPlusSquareFill className='text-2xl' />}
+					isActive={isOpen}
+					onClick={openHandler}
+				>
+					<BsPlusSquare className='text-2xl' />
+				</ActionIcon>
+			</li>
+			{createPortal(
+				content,
+				target,
+			)}
+		</>
+	);
+}
+
+
+export default CreateActionIconDialog; //Export main component
--- components/NavBar/_TabletNavBar.tsx
+++ components/NavBar/_TabletNavBar.tsx
@@ -1,17 +1,9 @@
-//React
-import { useState } from "react";
-import { createPortal } from "react-dom";
-
-
 //MATERIAL DESIGN
 //Components
 import ActionIconAnchor from "../ui/ActionIconAnchor";
 import ActionIcon from "../ui/ActionIcon";
-import Divider from "../ui/Divider";
 //Icons
-import {
-	MdClose,
-} from 'react-icons/md';
+
 import {
 	RiPolaroid2Line,
 } from 'react-icons/ri';
@@ -21,8 +13,6 @@ import {
 	BsCompassFill,
 	BsChatHeart,
 	BsChatHeartFill,
-	BsPlusSquare,
-	BsPlusSquareFill,
 	BsHeart,
 	BsHeartFill,
 } from 'react-icons/bs';
@@ -38,13 +28,7 @@ import {
 import {
 	BsSearch,
 } from 'react-icons/bs';
-import {
-	LiaPhotoVideoSolid,
-} from 'react-icons/lia';
-
-
-//Components
-import Dialog from 'native-react-dialog';
+import CreateActionIconDialog from "./_CreateActionIconDialog";
 
 
 //Typings
@@ -162,109 +146,7 @@ const NavList = (props: TabletNavBar) => {
 					<BsHeart className='text-2xl' />
 				</ActionIcon>
 			</li>
-			<CreateActionButton />
+			<CreateActionIconDialog />
 		</ul>
-	);
-}
-
-
-const CreateActionButton = () => {
-
-	//React
-	const [ isOpen, setIsOpen ] = useState<boolean>(false);
-
-	//Handlers
-	const openHandler = () => {
-		setIsOpen(true);
-	};
-
-	const closeHandler = () => {
-		setIsOpen(false);
-	};
-
-	const target = document.body;
-	const content = (
-		<Dialog
-			open={isOpen}
-			onClose={closeHandler}
-			className='rounded-md p-0 container max-w-2xl w-auto'
-			aria-label='Create dialog'
-			aria-description='Upload multimedia to post'
-		>
-			<header
-				className='relative p-2'
-				aria-label='Header'
-			>
-				<h1
-					className='text-center font-medium'
-					title='Create new post'
-					aria-label='Create new post'
-				>
-					Create new post
-				</h1>
-				<ActionIcon
-					onClick={closeHandler}
-					className='absolute right-4 top-2'
-					title='Close create dialog'
-					aria-label='Close create dialog button'
-					aria-description='Closes actual dialog'
-				>
-					<MdClose
-						className='fill-neutral-400 text-xl'
-						aria-label='Close icon'
-					/>
-				</ActionIcon>
-			</header>
-			<Divider
-				aria-label='Divider'
-				aria-description='Divides header from main'
-			/>
-			<main
-				className='p-2 flex flex-col gap-2 items-center justify-center h-96'
-				aria-label='Dialog main container'
-			>
-				<div
-					className='relative flex text-8xl'
-				>
-					<LiaPhotoVideoSolid
-						title='Pictures and videos'
-						aria-label='Pictures and videos'
-					/>
-				</div>
-				<span
-					className='text-xl font-light'
-					title='Drag here pictures and videos'
-					aria-label='Drag here pictures and videos'
-				>
-					Drag here pictures and videos
-				</span>
-				<button
-					className='bg-sky-500 text-sm rounded-md px-4 py-2 text-white font-medium active:bg-sky-700'
-					title='Select from computer'
-					aria-label='Select from computer'
-				>
-					Select from computer
-				</button>
-			</main>
-		</Dialog>
-	);
-
-	return(
-		<>
-			<li>
-				<ActionIcon
-					className={`w-full border border-transparent`}
-					activeIcon={<BsPlusSquareFill className='text-2xl' />}
-					isActive={isOpen}
-					onClick={openHandler}
-				>
-					<BsPlusSquare className='text-2xl' />
-				</ActionIcon>
-			</li>
-			{createPortal(
-				content,
-				target,
-			)}
-		</>
 	);
 }
