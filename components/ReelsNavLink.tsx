c40a180f88759feea8bbb726b145d5300515f4c0
Kykal <benavides.alan@hotmail.com>
2023-07-14 09:35:02 -0600

    Create ReelsNavLink.tsx

---
+++ components/ReelsNavLink.tsx
@@ -0,0 +1,20 @@
+//Typings
+type ReelsNavLink = {
+	isSectionActive: boolean;
+}
+
+
+//Main component content
+const ReelsNavLink = (props: ReelsNavLink): JSX.Element => {
+
+
+	//Main component render
+	return (
+		<>
+			
+		</>
+	);
+};
+
+
+export default ReelsNavLink; //Export main component
