8ea662a9da110fe4a782db9e302a72f91e1cbab0
Kykal <benavides.alan@hotmail.com>
2023-07-15 22:51:09 -0600

    Profile nav link added

---
+++ components/ProfileNavLink.tsx
@@ -0,0 +1,56 @@
+//NextJS
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+
+//Configurations
+import Routes from "@/configuration/routes";
+
+//Constants
+import { profilePictureUrl } from "@/constants";
+
+
+//Components
+import DynamicNavLink from "./DynamicNavLink";
+
+
+//Typings
+type ProfileNavLink = {
+	isSectionActive: boolean;
+}
+
+//Main component content
+const ProfileNavLink = ({isSectionActive}: ProfileNavLink): JSX.Element => {
+
+	//NextJS
+	const pathname = usePathname();
+
+	const icon = (
+		<Image
+			src={profilePictureUrl}
+			alt='Profile'
+
+			className='rounded-full aspect-square h-6 w-6'
+
+			width={50}
+			height={50}
+		/>
+	);
+
+	//Main component render
+	return (
+		<DynamicNavLink
+			url={Routes.PROFILE}
+
+			label='Profile'
+			
+			isSectionActive={isSectionActive}
+
+			icon={icon}
+			activeIcon={icon}
+		/>
+	);
+};
+
+
+export default ProfileNavLink; //Export main component
