cb1f65cae4a5240428c5d33ad236b65eb5eb77dd
Kykal <benavides.alan@hotmail.com>
2023-07-21 17:39:04 -0600

    Splitted menu lists

--- components/MoreButton/Menu.tsx
+++ components/MoreButton/Menu.tsx
@@ -21,55 +21,83 @@ import {
 
 //Components
 import ReportButton from './_ReportButton';
+import ChangeTheme from './_ChangeTheme';
+import Divider from '../UI/Divider';
 
 
 //Main component content
 const Menu = (): JSX.Element => {
 	//Main component render
 	return(
-		<menu
+		<>
+		<div
 			className='more-menu'
 		>
-			<li>
-				<Link
-					href={Routes.ACCOUNT_SETTINGS}
-					className='more-menu-item'
-					title='Settings'
-				>
-					<MdSettings className='text-2xl' />
-					<span>
-						Settings
-					</span>
-				</Link>
-			</li>
-			<li>
-				<Link
-					href={Routes.ACTIVITY}
-					className='more-menu-item'
-					title='Your Activity'
-				>
-					<TbHistoryToggle className='text-2xl' />
-					<span>
-						Your Activity
-					</span>
-				</Link>
-			</li>
-			<li>
-				<Link
-					href={Routes.SAVED}
-					className='more-menu-item'
-					title='Saved'
-				>
-					<PiFlagBanner className='text-2xl' />
-					<span>
-						Saved
-					</span>
-				</Link>
-			</li>
-			<li>
-				<ReportButton />
-			</li>
-		</menu>
+			<menu>
+				<li>
+					<Link
+						href={Routes.ACCOUNT_SETTINGS}
+						className='more-menu-item'
+						title='Settings'
+					>
+						<MdSettings className='text-2xl' />
+						<span>
+							Settings
+						</span>
+					</Link>
+				</li>
+				<li>
+					<Link
+						href={Routes.ACTIVITY}
+						className='more-menu-item'
+						title='Your Activity'
+					>
+						<TbHistoryToggle className='text-2xl' />
+						<span>
+							Your Activity
+						</span>
+					</Link>
+				</li>
+				<li>
+					<Link
+						href={Routes.SAVED}
+						className='more-menu-item'
+						title='Saved'
+					>
+						<PiFlagBanner className='text-2xl' />
+						<span>
+							Saved
+						</span>
+					</Link>
+				</li>
+				<li>
+					<ChangeTheme />
+				</li>
+				<li>
+					<ReportButton />
+				</li>
+			</menu>
+			<Divider />
+			<menu>
+				<li>
+					<button
+						className='more-menu-item'
+						disabled
+					>
+						Switch accounts
+					</button>
+				</li>
+				<li>
+					<button
+						className='more-menu-item'
+						disabled
+					>
+						Log out
+					</button>
+				</li>
+			</menu>
+		</div>
+		</>
 	);
 };
 
