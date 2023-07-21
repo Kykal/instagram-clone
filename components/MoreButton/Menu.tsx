daa0035e2a4cf336dd3dc048ec8d4c8ee04ac781
Kykal <benavides.alan@hotmail.com>
2023-07-21 17:44:43 -0600

    Update Menu.tsx

--- components/MoreButton/Menu.tsx
+++ components/MoreButton/Menu.tsx
@@ -30,73 +30,73 @@ const Menu = (): JSX.Element => {
 	//Main component render
 	return(
 		<>
-		<div
-			className='more-menu'
-		>
-			<menu>
-				<li>
-					<Link
-						href={Routes.ACCOUNT_SETTINGS}
-						className='more-menu-item'
-						title='Settings'
-					>
-						<MdSettings className='text-2xl' />
-						<span>
-							Settings
-						</span>
-					</Link>
-				</li>
-				<li>
-					<Link
-						href={Routes.ACTIVITY}
-						className='more-menu-item'
-						title='Your Activity'
-					>
-						<TbHistoryToggle className='text-2xl' />
-						<span>
-							Your Activity
-						</span>
-					</Link>
-				</li>
-				<li>
-					<Link
-						href={Routes.SAVED}
-						className='more-menu-item'
-						title='Saved'
-					>
-						<PiFlagBanner className='text-2xl' />
-						<span>
-							Saved
-						</span>
-					</Link>
-				</li>
-				<li>
-					<ChangeTheme />
-				</li>
-				<li>
-					<ReportButton />
-				</li>
-			</menu>
-			<Divider />
-			<menu>
-				<li>
-					<button
-						className='more-menu-item'
-						disabled
-					>
-						Switch accounts
-					</button>
-				</li>
-				<li>
-					<button
-						className='more-menu-item'
-						disabled
-					>
-						Log out
-					</button>
-				</li>
-			</menu>
-		</div>
+			<div
+				className='more-menu'
+			>
+				<menu>
+					<li>
+						<Link
+							href={Routes.ACCOUNT_SETTINGS}
+							className='more-menu-item'
+							title='Settings'
+						>
+							<MdSettings className='text-2xl' />
+							<span>
+								Settings
+							</span>
+						</Link>
+					</li>
+					<li>
+						<Link
+							href={Routes.ACTIVITY}
+							className='more-menu-item'
+							title='Your Activity'
+						>
+							<TbHistoryToggle className='text-2xl' />
+							<span>
+								Your Activity
+							</span>
+						</Link>
+					</li>
+					<li>
+						<Link
+							href={Routes.SAVED}
+							className='more-menu-item'
+							title='Saved'
+						>
+							<PiFlagBanner className='text-2xl' />
+							<span>
+								Saved
+							</span>
+						</Link>
+					</li>
+					<li>
+						<ChangeTheme />
+					</li>
+					<li>
+						<ReportButton />
+					</li>
+				</menu>
+				<Divider />
+				<menu>
+					<li>
+						<button
+							className='more-menu-item'
+							disabled
+						>
+							Switch accounts
+						</button>
+					</li>
+					<li>
+						<button
+							className='more-menu-item'
+							disabled
+						>
+							Log out
+						</button>
+					</li>
+				</menu>
+			</div>
 		</>
 	);
 };
