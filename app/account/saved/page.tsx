e5870d388bf617809c0fc5138c59668d2caad75b
Kykal <benavides.alan@hotmail.com>
2023-07-21 17:23:45 -0600

    Saved page added

---
+++ app/account/saved/page.tsx
@@ -0,0 +1,12 @@
+//Main component content
+const Page = (): JSX.Element => {
+	//Main component render
+	return (
+		<div>
+			Saved
+		</div>
+	);
+};
+
+
+export default Page; //Export main component
