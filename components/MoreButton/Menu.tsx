a630ee850d951461ef441c8bafa057cd8372040f
Kykal <benavides.alan@hotmail.com>
2023-07-22 00:16:36 -0600

    Switch appearance styles behaviour

--- components/MoreButton/Menu.tsx
+++ components/MoreButton/Menu.tsx
@@ -2,6 +2,10 @@
 import Link from 'next/link';
 
 
+//React
+import useDisclosure from '@/hooks/useDisclosure';
+
+
 //Configurations
 import Routes from '@/configuration/routes';
 
@@ -17,36 +21,59 @@ import {
 import {
 	PiFlagBanner,
 } from 'react-icons/pi';
+import {
+	BsChevronLeft,
+	BsSun,
+	BsMoon,
+} from 'react-icons/bs';
 
 
 //Components
 import ReportButton from './_ReportButton';
 import ChangeTheme from './_ChangeTheme';
 import Divider from '../UI/Divider';
+import IconButton from '../UI/IconButton';
+
+
+//Typings
+type MenuList = {
+	onThemeClick: () => void;
+}
+type MainOptions = {
+	onThemeClick: () => void;
+	showTheme: boolean;
+}
+type ThemeOptions = {
+	showTheme: boolean;
+	closeTheme: () => void;
+}
+
 
 
 //Main component content
 const Menu = (): JSX.Element => {
+
+	//React
+	const [ showThemeOptions, { open, close } ] = useDisclosure();
+
+
 	//Main component render
 	return(
 		<div
-			className='more-menu w-64'
+			className='more-menu'
 		>
 			<div
-				className='relative flex h-auto'
+				className='relative w-64 transition-all h-[345px] data-[theme-options=true]:h-[100px]'
+				data-theme-options={showThemeOptions}
 			>
-				<div
-					className='flex flex-col gap-2 w-full'
-				>
-					<MenuList />
-					<Divider />
-					<AuthMenu />
-				</div>
-				<div
-					className='absolute top-0 left-0 translate-x-full w-64'
-				>
-					a
-				</div>
+				<MainOptions
+					onThemeClick={open}
+					showTheme={showThemeOptions}
+				/>
+				<ThemeOptions
+					showTheme={showThemeOptions}
+					closeTheme={close}
+				/>
 			</div>
 		</div>
 	);
@@ -56,7 +83,8 @@ const Menu = (): JSX.Element => {
 export default Menu; //Export main component
 
 
-const MenuList = () => {
+
+const MenuList = (props: MenuList) => {
 	return(
 		<menu>
 			<li>
@@ -96,7 +124,9 @@ const MenuList = () => {
 				</Link>
 			</li>
 			<li>
-				<ChangeTheme />
+				<ChangeTheme
+					onClick={props.onThemeClick}
+				/>
 			</li>
 			<li>
 				<ReportButton />
@@ -126,5 +156,65 @@ const AuthMenu = () => {
 				</button>
 			</li>
 		</menu>
+	);
+}
+
+
+const MainOptions = (props: MainOptions) => {
+	
+	const translate = props.showTheme ? '-translate-x-full' : 'translate-x-0';
+
+	
+	return(
+		<div
+			className={`px-2 py-2.5 transition-all ${translate}`}
+		>
+			<div
+				className='flex flex-col gap-2 w-full'
+			>
+				<MenuList
+					onThemeClick={props.onThemeClick}
+				/>
+				<Divider />
+				<AuthMenu />
+			</div>
+		</div>
+	);
+}
+
+
+const ThemeOptions = (props: ThemeOptions) => {
+
+	const translate = props.showTheme ? 'translate-x-0' : 'translate-x-full';
+
+	return(
+		<div
+			className={`absolute px-2 py-2.5 transition-all top-0 left-0 ${translate} w-64 flex flex-col gap-2`}
+		>
+			<div
+				className='flex justify-between items-center h-7'
+			>
+				<IconButton
+					onClick={props.closeTheme}
+					className='h-full'
+				>
+					<BsChevronLeft />
+				</IconButton>
+				<span
+					className='font-medium h-full flex items-center justify-center'
+				>
+					Switch appearance
+				</span>
+				<BsSun className='h-full' />
+			</div>
+			<Divider />
+			<button
+				className='flex items-center justify-between p-2 hover:bg-neutral-100 rounded-md'
+			>
+				<span>
+					Dark mode
+				</span>
+			</button>
+		</div>
 	);
 }
--- components/MoreButton/_ChangeTheme.tsx
+++ components/MoreButton/_ChangeTheme.tsx
@@ -1,25 +1,29 @@
 //MATERIAL DESIGN
-//Components
 //Icons
 import {
 	BsSun,
 } from 'react-icons/bs';
 
 
+//Typings
+type ChangeTheme = {
+	onClick: () => void;
+}
+
+
 //Main component content
-const ChangeTheme = (): JSX.Element => {
+const ChangeTheme = (props: ChangeTheme): JSX.Element => {
 	//Main component render
 	return (
-		<>
-			<button
-				className='more-menu-item'
-			>
-				<BsSun className='text-2xl' />
-				<span>
-					Switch appeareance
-				</span>
-			</button>
-		</>
+		<button
+			className='more-menu-item'
+			onClick={props.onClick}
+		>
+			<BsSun className='text-2xl' />
+			<span>
+				Switch appeareance
+			</span>
+		</button>
 	);
 };
 
