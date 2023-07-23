3cdbd333eddd2775930fe8b6b357c8eef1b6a035
Kykal <benavides.alan@hotmail.com>
2023-07-22 21:21:27 -0600

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+//Main component content
+const Loading = (): JSX.Element => {
+
+
+	//Main component render
+	return (
+		<div
+			className='font-bold text-2xl'
+		>
+			Loading...
+		</div>
+	);
+};
+
+
+export default Loading; //Export main component
