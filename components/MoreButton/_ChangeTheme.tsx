eda6f34e189a288e73142ddeec3a98e3164d9209
Kykal <benavides.alan@hotmail.com>
2023-07-22 21:32:17 -0600

    Icon responds to page theme

--- components/MoreButton/_ChangeTheme.tsx
+++ components/MoreButton/_ChangeTheme.tsx
@@ -1,8 +1,15 @@
+//React
+import { useEffect, useState } from 'react';
+
+
 //MATERIAL DESIGN
 //Icons
+import { Theme } from '@/utils/theme/typings';
 import {
 	BsSun,
+	BsMoon,
 } from 'react-icons/bs';
+import getTheme from '@/utils/theme/getTheme';
 
 
 //Typings
@@ -13,13 +20,28 @@ type ChangeTheme = {
 
 //Main component content
 const ChangeTheme = (props: ChangeTheme): JSX.Element => {
+
+	//React
+	const [ theme, setTheme ] = useState<Theme>('light');
+
+	useEffect( () => {
+		const _theme = getTheme();
+
+		setTheme(_theme);
+	}, [] );
+
+	const dynamicIcon = theme === 'light'
+		?	<BsSun className='text-2xl' />
+		: <BsMoon className='text-2xl' />;
+
+		
 	//Main component render
 	return (
 		<button
 			className='more-menu-item'
 			onClick={props.onClick}
 		>
-			<BsSun className='text-2xl' />
+			{dynamicIcon}
 			<span>
 				Switch appearance
 			</span>
