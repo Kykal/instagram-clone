2099f503a9eb1e7e8365da52eafcd56b72c251f1
Kykal <benavides.alan@hotmail.com>
2023-07-28 17:56:38 -0600

    Create CreateMenuButton.tsx

---
+++ components/CreateMenuButton/CreateMenuButton.tsx
@@ -0,0 +1,19 @@
+//Typings
+type CreateMenuButton = {
+	opened: boolean;
+	close: () => void;
+}
+
+
+//Main component content
+const CreateMenuButton = (props: CreateMenuButton): JSX.Element => {
+	//Main component render
+	return (
+		<>
+			
+		</>
+	);
+};
+
+
+export default CreateMenuButton; //Export main component
