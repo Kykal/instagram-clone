f270a495f572420f48d63e623f78b92f7047a51e
Kykal <benavides.alan@hotmail.com>
2023-07-28 16:21:43 -0600

    Update ProfileNavLink.tsx

--- components/NavBar/ProfileNavLink.tsx
+++ components/NavBar/ProfileNavLink.tsx
@@ -43,7 +43,7 @@ const ProfileNavLink = ({isSectionActive}: ProfileNavLink): JSX.Element => {
 			src={profilePictureUrl}
 			alt='Profile'
 
-			className={`${baseIconStyles} border-white`}
+			className={`${baseIconStyles} border-2 border-white`}
 
 			width={50}
 			height={50}
