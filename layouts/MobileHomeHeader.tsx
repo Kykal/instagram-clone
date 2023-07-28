f591ea840e7709df5f482e2d264c4d80a1105245
Kykal <benavides.alan@hotmail.com>
2023-07-28 17:56:42 -0600

    Update MobileHomeHeader.tsx

--- layouts/MobileHomeHeader.tsx
+++ layouts/MobileHomeHeader.tsx
@@ -2,25 +2,44 @@
 'use client';
 
 
+//React
+import useDisclosure from '@/hooks/useDisclosure';
+
+
 //Components
-import SearchInput from '@/components/SearchInput';
 import NotificationAnchorIconButton from '@/components/NavBar/NotificationAnchorIconButton';
-import InstagramButton from '@/components/NavBar/InstagramButton';
+import Link from 'next/link';
 
 
 //Main component content
 const MobileHomeHeader = (): JSX.Element => {
+
+	//React
+	const [ opened, { open, close } ] = useDisclosure();
+
+
 	//Main component render
 	return (
 		<header
 			className='md:hidden absolute top-0 left-0 w-full border-b border-neutral-200 dark:border-neutral-800 bg-white dark:bg-black p-2'
 		>
 			<div
-				className='h-9 flex items-center gap-2'
+				className='h-9 flex items-center justify-between gap-2'
 			>
-				<InstagramButton />
-				<SearchInput />
-				<NotificationAnchorIconButton />
+				<h1
+					className='font-medium text-xl pl-2'
+				>
+					<Link
+						href='/'
+					>
+						Instagram
+					</Link>
+				</h1>
+				<div
+					className='h-full'
+				>
+					<NotificationAnchorIconButton />
+				</div>
 			</div>
 		</header>
 	);
