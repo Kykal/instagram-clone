73d6d523f6771f8167b0a59f75301c97c7fa4609
Kykal <benavides.alan@hotmail.com>
2023-07-28 18:35:38 -0600

    Create menu button

--- components/CreateMenuButton/CreateMenuButton.tsx
+++ components/CreateMenuButton/CreateMenuButton.tsx
@@ -1,16 +1,41 @@
-//Typings
-type CreateMenuButton = {
-	opened: boolean;
-	close: () => void;
-}
+//React
+import useDisclosure from "@/hooks/useDisclosure";
+
+
+//MATERIAL DESIGN
+//Components
+import IconButton from "../UI/IconButton";
+//Icons
+import {
+	BsPlusSquare,
+} from 'react-icons/bs';
+
+
+//Components
+import MenuPortal from "./MenuPortal";
 
 
 //Main component content
-const CreateMenuButton = (props: CreateMenuButton): JSX.Element => {
+const CreateMenuButton = (): JSX.Element => {
+
+	//React
+	const [ opened, { open, close, toggle } ] = useDisclosure();
+
+
 	//Main component render
 	return (
 		<>
-			
+			<IconButton
+				onClick={toggle}
+				className='h-full z-40'
+			>
+				<BsPlusSquare className='text-xl' />
+			</IconButton>
+			{opened && (
+				<MenuPortal
+					close={close}
+				/>
+			)}
 		</>
 	);
 };
