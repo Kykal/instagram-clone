d2fe59209b2c1e7e9ebf20eecb24420103447477
Kykal <benavides.alan@hotmail.com>
2023-07-30 20:13:09 -0600

    Stories page added

---
+++ app/stories/[id]/page.tsx
@@ -0,0 +1,12 @@
+//Main component content
+const Page = (): JSX.Element => {
+	//Main component render
+	return (
+		<div>
+			Story page!
+		</div>
+	);
+};
+
+
+export default Page; //Export main component
