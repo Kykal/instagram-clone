916a13fd45fa983df7e731f3fd26a8ca115f73af
Kykal <benavides.alan@hotmail.com>
2023-08-02 01:53:03 -0600

    Aside suggested finished

--- layouts/AsideBar/SuggestedForYou.tsx
+++ layouts/AsideBar/SuggestedForYou.tsx
@@ -1,6 +1,17 @@
 //NextJS
-import Routes from "@/configuration/routes";
 import Link from "next/link";
+
+
+//Configurations
+import Routes from "@/configuration/routes";
+
+
+//Components
+import SuggestedProfile from "./_SugestedProfile";
+
+
+//Typings
+import type { SuggestedProfile as SuggestedProfileType } from './_SugestedProfile';
 
 
 //Main component content
@@ -20,14 +31,68 @@ const SuggestedForYou = (): JSX.Element => {
 				</span>
 				<Link
 					href={Routes.EXPLORE_PEOPLE}
-					className='hover:text-neutral-500'
+					className='hover:text-neutral-500 text-xs font-semibold'
 				>
 					See All
 				</Link>
 			</div>
+			<SuggestedProfiles />
 		</div>
 	);
 };
 
 
 export default SuggestedForYou; //Export main component
+
+
+
+const SuggestedProfiles = (): JSX.Element => {
+
+	const users: SuggestedProfileType[] = [
+		{
+			followedBy: 'its_my_other_me',
+			user: {
+				hasStories: false,
+				name: 'its_me',
+				imgUrl: 'https://dummyimage.com/500x500/000/fff.png',
+			}
+		},
+		{
+			followedBy: 'ligo_leyen',
+			user: {
+				hasStories: true,
+				name: 'rito_games',
+				imgUrl: 'https://dummyimage.com/500x500/000/fff.png',
+			}
+		},
+		{
+			followedBy: 'vue',
+			user: {
+				hasStories: false,
+				name: 'angular',
+				imgUrl: 'https://dummyimage.com/500x500/000/fff.png',
+			}
+		},
+		{
+			followedBy: 'vercel',
+			user: {
+				hasStories: false,
+				name: 'adjacentjs',
+				imgUrl: 'https://dummyimage.com/500x500/000/fff.png',
+			}
+		}
+	];
+
+	return (
+		<div
+			className='flex flex-col gap-4'
+		>
+			{users.map( (user, index) => (
+				<SuggestedProfile
+					key={`suggested-profile-${index}`}
+					{...user}
+				/>
+			) )}
+		</div>
+	);
+};
