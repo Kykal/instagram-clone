d531b54b9f6cfa39d6c256cf8ad1af7ba951dff5
Kykal <benavides.alan@hotmail.com>
2023-08-02 23:14:37 -0600

    Enhanced? Avatar

--- components/UserAvatar/UserAvatar.tsx
+++ components/UserAvatar/UserAvatar.tsx
@@ -11,20 +11,25 @@ import Routes from "@/configuration/routes";
 //Main component content
 const UserAvatar = (props: User): JSX.Element => {
 
-	const href = `${Routes.STORIES}/${props.name}`;
+	const storiesHref = `${Routes.STORIES}/${props.name}`;
+	const profileHref = `/${props.name}`;
+
+
+	const storiesBorder = (
+		<>
+			<div className='avatar-gradient' />
+			<div className='avatar-gradient--transparency' />
+		</>
+	);
+	
 
 	//Main component render
 	return (
 		<Link
 			className='avatar-container'
-			href={href}
+			href={props.hasStories ? storiesHref : profileHref}
 		>
-			{props.hasStories && (
-				<>
-					<div className='avatar-gradient' />
-					<div className='avatar-gradient--transparency' />
-				</>
-			)}
+			{props.hasStories && storiesBorder}
 			<Image
 				src={props.imgUrl}
 				alt='Profile'
