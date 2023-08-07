2394aabb10783e1b3821f581be17197775750b96
Kykal <benavides.alan@hotmail.com>
2023-08-07 11:22:09 -0600

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -2,15 +2,19 @@ class User {
 	readonly username: string;
 	readonly name?: string;
 	readonly avatarUrl: string;
+	readonly hasStories: boolean;
+
 
 	constructor(
 		username: string,
 		avatar_url: string,
+		has_stories: boolean,
 
 		name?: string,
 	) {
 		this.username = username;
 		this.avatarUrl = avatar_url;
+		this.hasStories = has_stories;
 		this.name = name;
 	}
 }
