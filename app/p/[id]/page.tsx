3b86cd05bc427df08dcf5f456390c714acff0f83
Kykal <benavides.alan@hotmail.com>
2023-08-08 23:53:16 -0600

    Create page.tsx

---
+++ app/p/[id]/page.tsx
@@ -0,0 +1,18 @@
+//Typings
+type Page = {
+	
+}
+
+
+//Main component content
+const Page = (props: Page): JSX.Element => {
+	//Main component render
+	return (
+		<div>
+			Post!
+		</div>
+	);
+};
+
+
+export default Page; //Export main component
