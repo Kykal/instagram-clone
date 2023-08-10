29f269a1240654283b133d493b0782b67233b93d
Kykal <benavides.alan@hotmail.com>
2023-08-09 23:30:42 -0600

    Create Dialog.tsx

---
+++ components/UI/Dialog.tsx
@@ -0,0 +1,26 @@
+import NativeDialog from 'native-react-dialog';
+
+
+//Typings
+type Dialog = {
+	opened: boolean;
+	onClose: () => void;
+
+	children?: any;
+}
+
+
+//Main component content
+const Dialog = (props: Dialog): JSX.Element => {
+
+
+	//Main component render
+	return (
+		<>
+			
+		</>
+	);
+};
+
+
+export default Dialog; //Export main component
