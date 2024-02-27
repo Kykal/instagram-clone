04809b87e6993199ba3e4adb55fcf1d6831d6d3e
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2024-02-27 10:45:23 -0600

    x

--- app/components/Avatar/Avatar.tsx
+++ app/components/Avatar/Avatar.tsx
@@ -1,6 +1,6 @@
 //NextJS
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 //Styles
@@ -25,20 +25,23 @@ const Avatar = (props: AvatarProps): JSX.Element => {
 		?	`${props.displayName.substring(0, MAX_LENGTH)}...`
 		: props.displayName;
 
-
+	
 	//Main component render
 	return (
 		<Link
 			className='avatar-container'
 			href={`/stories/${props.displayName}`}
 		>
-			<Image
-				className='avatar-profile'
-				alt={props.displayName}
-				src={props.imgUrl}
-				width={200}
-				height={200}
-			/>
+			<div className='relative' >
+				<Image
+					className='avatar-profile'
+					alt={props.displayName}
+					src={props.imgUrl}
+					width={200}
+					height={200}
+				/>
+				{props.hasStory && <StoryBackground /> }
+			</div>
 			<span className='avatar-display-name' >
 				{displayName}
 			</span>
@@ -48,3 +51,16 @@ const Avatar = (props: AvatarProps): JSX.Element => {
 
 
 export default Avatar; //Export main component
+
+
+const StoryBackground = () => {
+	return (
+		<div
+			className='absolute top-0 left-0 h-14 w-14 flex items-center justify-center'
+		>
+			<div
+				className='avatar-story-background'
+			/>
+		</div>
+	);
+}
